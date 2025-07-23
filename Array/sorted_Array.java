578371159d7a219a4e422517d3bef8c08d1a489c
PremVignesh15 <145543379+PremVignesh15@users.noreply.github.com>
2025-07-23 20:37:29 +0530

    Add files via upload

---
+++ Array/sorted_Array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class IsSorted {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        boolean isSorted = true;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("Array is sorted in ascending order.");
+        } else {
+            System.out.println("Array is NOT sorted.");
+        }
+    }
+}
